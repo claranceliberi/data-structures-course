690f1fbb976f4c5ea104b37d65e08b85453cd03d
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-05-04 07:35:00 +0200

    feat:enque circular que

--- Data Structures/Queue/arrayCirculaQue.cpp
+++ Data Structures/Queue/arrayCirculaQue.cpp
@@ -24,10 +24,30 @@ int isEmpty(){
     return 0;
 }
 
+// add element to que
+int enQueue(int *queue, int data){
+    //check over flow
+    if(isFull()){
+        cout << "Queue overflow" << endl;
+        return 0;
+    }
+
+    if(isEmpty())
+        front = 0;
+    rear = (rear + 1) % SIZE;
+    queue[rear] = data;
+
+    return 1;
+}
 
 int main(){
     int* queue = createCQueue(SIZE);
+    enQueue(queue,3);
+    enQueue(queue,4);
+    enQueue(queue,1);
+    enQueue(queue,23);
 
-    cout << isFull() << " and " << isEmpty() << endl;
+//    cout << queue[0] << endl;
+//    cout << isFull() << " and " << isEmpty() << endl;
     return 0;
 }
