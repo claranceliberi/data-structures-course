cc775af68c5d3eeb157c085368a4c1d14da99536
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-05-04 07:28:05 +0200

    feat: check empty and full of circular que

--- Data Structures/Queue/arrayCirculaQue.cpp
+++ Data Structures/Queue/arrayCirculaQue.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
 using namespace std;
 
+#define SIZE 5
+int front = -1, rear = -1;
+
+//create and return array of circular que
 int* createCQueue(int size){
     int *array = new int[size] ;
     return array;
 }
 
-int main(){
-    int* queue = createCQueue(10);
+//check whether circular que is full
+int isFull(){
+    if((front == 0 && rear == SIZE - 1) || (front == rear + 1))
+        return 1;
+    return 0;
+}
 
+//check whether que is full or empty
+int isEmpty(){
+    if(front == -1 && rear == -1)
+        return 1;
+    return 0;
+}
+
+
+int main(){
+    int* queue = createCQueue(SIZE);
+
+    cout << isFull() << " and " << isEmpty() << endl;
     return 0;
 }
