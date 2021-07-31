a9fbdc2e3aa6d1563050e3ea1fe2fae32a1ffe63
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-07-31 17:05:07 +0200

    feat: insert at end and begining

--- Data Structures/LinkedList/python/LinkedList.py
+++ Data Structures/LinkedList/python/LinkedList.py
@@ -3,40 +3,41 @@ class Node:
     def __init__(self,item):
         self.item = item
         self.next = None
-
+ 
 class LinkedList:
     
     def __init__(self) :
-        self.head = None
+        self.head : Node = None
 
-def show_list(ll : LinkedList):
-    list_to_show = ll
-    while list_to_show.head != None:
-        print(list_to_show.head.item, ", ")
-        list_to_show.head = list_to_show.head.next
+    # printing 
+    def show_list(self ):
+        print_list : Node = self.head
 
-def insert(data : int, ll : LinkedList = None) -> LinkedList:
-    mimicList : LinkedList = ll
-    if(mimicList.head == None):
-        mimicList.head = Node(data)
-    else :
-        while(mimicList.head !=None):
-            mimicList.head = mimicList.head.next
-        mimicList.item = data
-        return ll
+        while print_list is not None:
+            print(print_list.item)
+            print_list = print_list.next
+
+    def insert_at_beg(self,data ):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_at_end(self,data):
+        list = self.head
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+        else:
+            while list.next is not None:
+                list = list.next
+
+            list.next = new_node
+
+
 
 if __name__ == "__main__":
     linked_list = LinkedList()
-
-    # #defining nodes
-    # linked_list.head = Node(1)
-    # second = Node(2)
-    # third = Node(3)
-
-    # #connecting nodes
-    # linked_list.head.next = second
-    # second.next = third
-
     toContinue = True
     choice = 0
 
@@ -44,15 +45,18 @@ if __name__ == "__main__":
 
     
     while(toContinue):
-        print (" 1. show \n 2. insert \n 3. exit \n \n \n")
+        print (" 1. show \n 2. insert at beginning \n 3. insert at end  \n 4. exit \n \n \n")
         choice = int(input("Enter choice : "))
 
         if choice == 1:
-            show_list(linked_list)
+            linked_list.show_list()
         elif choice == 2 : 
             data = input("enter data : ")
-            insert(data,linked_list)
+            linked_list.insert_at_beg(data)
         elif choice == 3 : 
+            data = input("enter data : ")
+            linked_list.insert_at_end(data)
+        elif choice == 4 : 
             toContinue = False
         else :
             print(' wront choice try again \n\n')
